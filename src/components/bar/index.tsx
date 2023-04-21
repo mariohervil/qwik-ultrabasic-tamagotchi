b346750ba31637eb640637fabd32752a1c63c0e2
mariohervil <mariohervil@campus.monlau.com>
2023-04-21 19:21:45 +0200

    Componente Bar

--- src/components/bar/index.tsx
+++ src/components/bar/index.tsx
@@ -1,7 +1,37 @@
-import { component$, useSignal } from "@builder.io/qwik";
+import { component$, useStore, useVisibleTask$ } from "@builder.io/qwik";
 import { BarProps } from "~/interfaces/barprops";
 
-export const bar = component$<BarProps>((props) => {
-  const count = useSignal(0);
-  return <div onClick$={(ev) => {}}></div>;
+export const Bar = component$<BarProps>((props) => {
+  const barColor = useStore({
+    color: "lightgreen",
+  });
+
+  useVisibleTask$(({ track }) => {
+    console.log(barColor.color);
+    track(() => props.value);
+    if (props.value >= 60) {
+      barColor.color = "lightgreen";
+    } else if (props.value < 60 && props.value > 30) {
+      barColor.color = "yellow";
+    } else {
+      barColor.color = "red";
+    }
+  });
+
+  return (
+    <div class="bar-container">
+      <div class="bar-label">{props.stat}</div>
+      <div class="bar">
+        <div
+          style={{
+            width: `${props.value}%`,
+            backgroundColor: `${barColor.color}`,
+            height: "100%",
+            transition: "width 0.5s ease",
+          }}
+        ></div>
+      </div>
+      <div class="bar-value">{props.value}%</div>
+    </div>
+  );
 });
